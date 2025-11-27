c3be2c72f3acfd8be9f2ab867ca7d8dda2abd42f
aktan228 <aktam200e@gmail.com>
2025-11-28 02:22:50 +0600

    добавил urls.py и прописал маршруты

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
+    "rest_framework",
+    "drf_spectacular",
+    "django_filters",
     "presscentre",
 ]
 
@@ -49,8 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
-    
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"

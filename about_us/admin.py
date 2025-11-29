9d4ddb9ee9083c8a18a6f111f47ce57fdaec70b6
adilhan <adilhansatymkulov40@gmail.com>
2025-11-29 13:27:41 +0600

    Register Leader model in admin panel

--- about_us/admin.py
+++ about_us/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import FactCard, FactDetail
+from .models import FactCard, FactDetail, Leader
 
 admin.site.register(FactCard)
 admin.site.register(FactDetail)
+admin.site.register(Leader)

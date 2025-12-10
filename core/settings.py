db2b20c86c8a3745dbcd180a888b002372adfb31
IbroIT <ibroabdraimov20@gmail.com>
2025-12-10 16:58:27 +0600

    Fix INSTALLED_APPS syntax error in settings.py

--- core/settings.py
+++ core/settings.py
@@ -49,28 +49,6 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     "drf_spectacular",
-
-# Application definition
-
-INSTALLED_APPS = [
-    "unfold",
-    "unfold.contrib.filters",
-    "unfold.contrib.forms",
-    "unfold.contrib.inlines",
-    "unfold.contrib.import_export",
-    "unfold.contrib.guardian",
-    "unfold.contrib.simple_history",
-    "unfold.contrib.location_field",
-    "unfold.contrib.constance",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "corsheaders",
-    "rest_framework",
-    "drf_spectacular",
     "django_filters",
     "presscentre",
     "about_us",

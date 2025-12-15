00ed806ce399b660b8f491f94b59c77e5d77a2b4
adilhan <adilhansatymkulov40@gmail.com>
2025-12-15 10:13:02 +0600

    c

--- Gallery/admin.py
+++ Gallery/admin.py
@@ -9,18 +9,10 @@ class GalleryInline(admin.TabularInline):
 
 @admin.register(Category)
 class CategoryAdmin(ModelAdmin):
-    inlines = [GalleryInline]  # Здесь можно добавлять фото к категории
-    list_display = ['name', 'image_count']
-    
-    def image_count(self, obj):
-        return obj.images.count()
+    pass
 
 @admin.register(Gallery)
 class GalleryAdmin(ModelAdmin):
-    list_display = ['id', 'category', 'image_preview']
+    inlines = [GalleryInline]  # Здесь можно добавлять фото к категории
+    list_display = ['name', 'image_count']
     
-    def image_preview(self, obj):
-        if obj.image:
-            return f'<img src="{obj.image.url}" style="max-height: 50px;" />'
-        return "Нет изображения"
-    image_preview.allow_tags = True

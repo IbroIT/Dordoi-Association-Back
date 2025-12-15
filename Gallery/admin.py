81e75bdc6b682e415259214510aebe0ddb032edc
adilhan <adilhansatymkulov40@gmail.com>
2025-12-15 09:39:27 +0600

    c

--- Gallery/admin.py
+++ Gallery/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Category, Gallery
-# Register your models here.
+from unfold.admin import ModelAdmin
 
-admin.site.register(Category)
-admin.site.register(Gallery)
+
+@admin.register(Category)
+class CustomAdminClass(ModelAdmin):
+    pass
+@admin.register(Gallery)
+class CustomAdminClass(ModelAdmin):
+    pass

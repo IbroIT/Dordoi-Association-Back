a5c46e058e233f576bffd7cedd42d941a4f9c0e7
IbroIT <ibroabdraimov20@gmail.com>
2025-12-18 14:59:51 +0600

    sadfs

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,13 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'https://localhost:8000',
+    'https://127.0.0.1:8000',
+]
+
 # Application definition
 
 INSTALLED_APPS = [

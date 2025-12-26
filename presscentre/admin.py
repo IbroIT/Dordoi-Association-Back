e257b36ad44552175d6acdd4a28eb0ae18e7dfe6
IbroIT <ibroabdraimov20@gmail.com>
2025-12-26 12:47:06 +0600

    Add Category, News and NewsPhoto admin interfaces

--- presscentre/admin.py
+++ presscentre/admin.py
@@ -15,7 +15,67 @@ class PublicationAdmin(ModelAdmin):
         ("Даты", {"fields": ("created_at", "updated_at")}),
     )
 
+@admin.register(Category)
+class CategoryAdmin(ModelAdmin):
+    list_display = ["id", "get_title_ru", "created_at"]
+    search_fields = ["title_ru", "title_en", "title_kg"]
+    readonly_fields = ["created_at", "updated_at"]
+
+    fieldsets = (
+        ("Название", {"fields": ("title_ru", "title_en", "title_kg")}),
+        ("Даты", {"fields": ("created_at", "updated_at")}),
+    )
+
+    def get_title_ru(self, obj):
+        return obj.title_ru
+
+    get_title_ru.short_description = "Название (RU)"
+
+
+@admin.register(News)
+class NewsAdmin(ModelAdmin):
+    list_display = ["id", "get_title_ru", "category", "is_banner", "is_recommended", "published_at", "created_at"]
+    search_fields = ["title_ru", "title_en", "title_kg", "description_ru"]
+    readonly_fields = ["created_at", "updated_at"]
+    list_filter = ["category", "is_banner", "is_recommended", "published_at"]
+    list_editable = ["is_banner", "is_recommended"]
+
+    fieldsets = (
+        ("Основная информация", {
+            "fields": ("title_ru", "title_en", "title_kg", "category", "is_banner", "is_recommended")
+        }),
+        ("Краткое описание", {
+            "fields": ("short_description_ru", "short_description_en", "short_description_kg")
+        }),
+        ("Полное описание", {
+            "fields": ("description_ru", "description_en", "description_kg")
+        }),
+        ("Изображение", {
+            "fields": ("image", "cropping")
+        }),
+        ("Публикация", {
+            "fields": ("published_at",)
+        }),
+        ("Даты", {
+            "fields": ("created_at", "updated_at")
+        }),
+    )
+
     def get_title_ru(self, obj):
         return obj.title_ru
 
     get_title_ru.short_description = "Заголовок (RU)"
+
+
+@admin.register(NewsPhoto)
+class NewsPhotoAdmin(ModelAdmin):
+    list_display = ["id", "news", "order", "created_at"]
+    search_fields = ["news__title_ru"]
+    readonly_fields = ["created_at", "updated_at"]
+    list_filter = ["news"]
+
+    fieldsets = (
+        ("Новость", {"fields": ("news",)}),
+        ("Изображение", {"fields": ("image", "order")}),
+        ("Даты", {"fields": ("created_at", "updated_at")}),
+    )
